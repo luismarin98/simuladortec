9a51fa04290657010e492cea9ecd2a69976cee84
AndyMDV <jun19981708@hotmail.com>
2023-08-09 23:00:02 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import Layout from '@/components/Layout'
-import React from 'react'
+import * as React from 'react'
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 export default function Home() {
   React.useEffect(() => { document.title = 'Simulador | Inicio' }, [])
   return (
-    <Layout>¡Bienvenido al Simulador de Preguntas definitivo! Sumérgete en desafíos de conocimiento que pondrán a prueba tu perspicacia en una variedad de temas. Desde curiosidades generales hasta detalles especializados, esta es tu oportunidad de demostrar tus habilidades. ¿Listo para comenzar? ¡A jugar!</Layout>
+    <Layout>
+      <Card sx={{ width: '450px' }}>
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">Absoluta discreción</Typography>
+          <Typography gutterBottom variant="subtitle1" component='div'>Tuto!</Typography>
+          <Typography variant="body2" color="text.secondary">
+            En la parte izquierda tendras las opciones de un menu para poder hacer uso del simulador, tambien tendras la opciones de escoger modos en donde podras comenzar
+            a usar el simulador con preguntas aleatorias y ordenadas y abajo el cuestionario de las preguntas.
+          </Typography>
+        </CardContent>
+      </Card>
+    </Layout>
   )
 }
